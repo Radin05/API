a3d3a9c8a20a01bab31b2b4605a8fc97b523de62
radin05 <uhuuuykece07@gmail.com>
2024-08-02 08:45:10 +0700

    layout

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,13 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\FanController;
 use App\Http\Controllers\Api\KlubController;
 use App\Http\Controllers\Api\LigaController;
+use App\Http\Controllers\Api\FanController;
 use App\Http\Controllers\Api\PemainController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -25,19 +15,18 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
-
-    // matic
-    Route::resource('liga', LigaController::class)->except(['edit', 'create']);
-    Route::resource('klub', KlubController::class)->except(['edit', 'create']);
-    Route::resource('pemain', PemainController::class)->except(['edit', 'create']);
-    Route::resource('fan', FanController::class)->except(['edit', 'create']);
+    Route::resource('liga', LigaController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::resource('liga', LigaController::class)->except(['create', 'edit']);
+    Route::resource('klub', KlubController::class)->except(['create', 'edit']);
+    Route::resource('pemain', PemainController::class)->except(['create', 'edit']);
+    Route::resource('fan', FanController::class)->except(['create', 'edit']);
 });
 
-Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
-// Route::get('liga', [LigaController::class, 'index']);
-// Route::post('liga', [LigaController::class, 'store']);
-// Route::get('liga/{id}', [LigaController::class, 'show']);
-// Route::put('liga/{id}', [LigaController::class, 'update']);
-// Route::delete('liga/{id}', [LigaController::class, 'destroy']);
+// Route::get('liga',[LigaController::class,'index']);
+// Route::post('liga',[LigaController::class,'store']);
+// Route::get('liga/{id}',[LigaController::class,'show']);
+// Route::put('liga/{id}',[LigaController::class,'update']);
+// Route::delete('liga/{id}',[LigaController::class,'destroy']);
